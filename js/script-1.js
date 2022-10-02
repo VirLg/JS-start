c4df25ac733895e5932595ad92237be5141cec24
VirLg <108211942+VirLg@users.noreply.github.com>
2022-10-02 22:58:32 +0300

    hw

---
+++ js/script-1.js
@@ -0,0 +1,62 @@
+// const a  = 'переменная a';
+// console.log(a);
+
+// const userName = "asasasass"
+
+// console.log('Учим js');
+// ['учим', 'JS'].foeEach(alert);
+
+// const num = 12;
+// let rez;
+// rez = (num >= 18 && num > 18);
+
+
+// // if (num >= 0) {
+// // 	let res = 1;
+// // } else {
+// // 	let res = 2;
+// // }
+
+// console.log(rez);
+
+// const car = { 
+//     a: 10,
+//     b: true,
+// }
+
+// const carN = car;
+
+// carN.a = 20;
+// console.log(car.a);
+
+
+// const mySity = {
+//     sity: 'New-York'
+// }
+
+
+
+// console.log(mySity);
+
+// mySity.sity = 'las-vegas';
+
+// console.log(mySity);
+// mySity.popular = true;
+// console.log(mySity.popular);
+// mySity.house = 12;
+// console.log(mySity.house);
+// mySity.contry = "USA";
+// console.log(mySity.contry);
+// console.log(mySity);
+// delete mySity.contry;
+// console.log(mySity)
+
+
+// const myCar = {
+//     tradeName: "VW",
+//     colorCar: function () { 
+//         console.log('white')
+//     }
+// }
+
+// myCar.colorCar();
